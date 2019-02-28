29462fc799a29f6878725ebb605e2a4fe98ce06e
August Lund Eilertsen <le_lund@hotmail.com>
2019-02-27 17:24:27 -0800

    fix indent

--- pbApi/settings.py
+++ pbApi/settings.py
@@ -17,17 +17,17 @@ AUTH_USER_MODEL = 'user.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-	'rest_framework.authtoken',
-    'weather_data',
-	'user',
-	'raw'
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'rest_framework',
+    'rest_framework.authtoken',
+	'weather_data',
+    'user',
+    'raw'
 ]
 
 MIDDLEWARE = [
